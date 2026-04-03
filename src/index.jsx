5a1ca067498811aaa48846ad28c9a8e6f8bf963d
Kitesurfera <claudiakiter31@gmail.com>
2026-04-03 02:27:07 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,6 @@
 import { registerRoot } from "remotion";
 import { RemotionVideo } from "./Video.jsx";
+import { RemotionCarrusel } from "./Carrusel.jsx";
 
 registerRoot(RemotionVideo);
+registerRoot(RemotionCarrusel);
