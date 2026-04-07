3e89c4813ecc732068b52d2848d1bf33d4450ae4
Kitesurfera <claudiakiter31@gmail.com>
2026-04-07 19:48:08 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { registerRoot } from "remotion";
 import { RemotionVideo } from "./Video.jsx";
 import { RemotionCarrusel } from "./Carrusel.jsx";
+import { RemotionPlan } from "./Plan.jsx"; // <-- AÑADIDO
 
-// Agrupamos todos los agentes visuales en una sola raíz
 const RaizRemotion = () => {
   return (
     <>
       <RemotionVideo />
       <RemotionCarrusel />
+      <RemotionPlan /> 
     </>
   );
 };
